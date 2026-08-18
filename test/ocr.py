c22d0d85a555a81502bad2cca626eda134af89fd
cuixiao-yu <591907060@qq.com>
2026-08-18 19:58:20 +0800

    test(ocr): 编写 OCR 的测试脚本

---
+++ test/ocr.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('src')
+
+from word_api import capture, extract
+
+img = capture()
+
+if img:
+    texts = extract(img)
+
+    for i, text in enumerate(texts):
+        print(f'{i+1}: {text}')
